1d17417b59747b1976dc4f0524f42cbdde6ef3dc
bugraozkaya <ahmetbugraozkaya14@gmail.com>
2026-07-06 08:59:52 +0300

    Baslangic: bos ASP.NET Core MVC projesi

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PersonelYonetim.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
